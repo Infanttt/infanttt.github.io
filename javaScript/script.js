b9512346bdb527a916e5a2db7bad889176413967
Infanttt <94824650+Infanttt@users.noreply.github.com>
2022-05-17 18:11:11 +0700

    Adding clearence slip

--- javaScript/script.js
+++ javaScript/script.js
@@ -4,12 +4,25 @@ function openBut() {
   document.getElementById("mainContent").style.marginLeft = "300px";
   document.getElementById("menuBut").style.display = "none";
 }
+
 function closeBut() {
   document.getElementById("navig").style.marginLeft = "-300px";
   document.getElementById("mainContent").style.marginLeft = "0px";
   document.getElementById("menuBut").style.display = "block";
 }
+
 function sTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function myFunction(x) {
+  if (x.matches) {
+    document.getElementById("navig").style.marginLeft = "-300px";
+    document.getElementById("menuBut").style.display = "block";
+  }
+}
+
+let x = window.matchMedia("(max-width: 800px)")
+myFunction(x)
+x.addListener(myFunction)
